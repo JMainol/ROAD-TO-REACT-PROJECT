a83d6a3186b1ec1d0cc21aaa983e53aefdf208cb
JMainol-Git <jmcharrogarcia@gmail.com>
2019-10-09 21:16:31 +0200

    Input filtrado resultados operativo y simplificado con ES6 segun pag 58

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ const list = [
   },
 ];
 
+const isSearch = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props) {
@@ -27,9 +30,15 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event){
+    this.setState({searchTerm: event.target.value});
   }
 
   onDismiss(id){
@@ -40,9 +49,16 @@ class App extends Component {
 
 
   render() {
+    const {searchTerm, list} = this.state;
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input 
+          type="text"
+          onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearch(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}></a>

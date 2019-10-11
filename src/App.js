8b52e71e3b381c851f40c367e66cff836f9f9b54
JMainol-Git <jmcharrogarcia@gmail.com>
2019-10-11 17:57:08 +0200

    estilo css inline con consts pag 77

--- src/App.js
+++ src/App.js
@@ -51,13 +51,15 @@ class App extends Component {
   render() {
     const {searchTerm, list} = this.state;
     return (
-      <div className="App">
-        <Search
-         value={searchTerm}
-         onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
         <Table
           list={list}
           pattern={searchTerm}
@@ -81,28 +83,44 @@ const Search = ({value, onChange, children}) => {
     );
 }
 
-const Table = ({list, pattern, onDismiss}) => {
-  
-    return (
-      <div>
+
+const largeColumn = {
+  width: '40%',
+};
+const midColumn = {
+  width: '30%',
+};
+const smallColumn = {
+  width: '10%',
+};
+
+const Table = ({list, pattern, onDismiss}) => 
+    <div className="table">
         {list.filter(isSearch(pattern)).map(item=>
-        <div key={item.objectID}>
-          <span>
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
           </span> 
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <Button onClick={() => onDismiss(item.objectID)}>
+          <span style={midColumn}>
+          {item.author}
+          </span>
+          <span style={smallColumn}>
+          {item.num_comments}
+          </span>
+          <span style={smallColumn}>
+          {item.points}
+          </span>
+          <span style={smallColumn}>
+            <Button 
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+            >
               Dismiss
             </Button>
           </span>
         </div>
       )}
     </div>
-    );
-  }
 
 const Button = ({onClick, className, children}) => {
   

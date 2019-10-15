fc8c0fe267766cee86c4b6ff9cc732344e5563e0
JMainol-Git <jmcharrogarcia@gmail.com>
2019-10-15 18:48:17 +0200

    Integration of error handling pag 112

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -82,7 +83,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({error}));
   }
 
   componentDidMount() {
@@ -102,7 +103,7 @@ class App extends Component {
     if (this.needsToSearchTopStories(searchTerm)){
       this.fetchSearchTopStories(searchTerm);
     }
-    
+
     event.preventDefault();
   }
 
@@ -125,7 +126,8 @@ class App extends Component {
     const {
       searchTerm, 
       results,
-      searchKey
+      searchKey,
+      error
     } = this.state;
 
     const page = (
@@ -139,7 +141,7 @@ class App extends Component {
       results[searchKey] &&
       results[searchKey].hits
     ) || [];
-
+    
     return (
       <div className="page">
         <div className="interactions">
@@ -151,10 +153,15 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-            list={list}
-            onDismiss={this.onDismiss}
-        />
+        { error 
+          ? <div className="interactions">
+              <p>Something went wrong.</p>
+            </div>
+          :  <Table
+              list={list}
+              onDismiss={this.onDismiss}
+            />
+        }
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More

8bcf1ff66d78c8dbc15d13375feabcfbd47dac51
JMainol-Git <jmcharrogarcia@gmail.com>
2019-10-21 21:58:38 +0200

    PropTpe integrado libro pag 139

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -200,6 +201,13 @@ const smallColumn = {
   width: '10%',
 };
 
+Search.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
 const Table = ({list, onDismiss}) => 
     <div className="table">
         {list.map(item=>
@@ -228,9 +236,24 @@ const Table = ({list, onDismiss}) =>
       )}
     </div>
 
-const Button = ({onClick, className, children}) => {
-  
-    return (
+    Table.propTypes = {
+      list: PropTypes.arrayOf(
+        PropTypes.shape({
+          objectID: PropTypes.string.isRequired,
+          author: PropTypes.string,
+          url: PropTypes.string,
+          num_comments: PropTypes.number,
+          points: PropTypes.number,
+        })
+      ).isRequired,
+      onDismiss: PropTypes.func.isRequired,
+    };
+
+const Button = ({
+  onClick, 
+  className,
+  children,
+}) => 
       <button
         onClick={onClick}
         className={className}
@@ -238,8 +261,17 @@ const Button = ({onClick, className, children}) => {
       >
         {children}
       </button>
-    );
-}
+
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
 
 export default App;
 
